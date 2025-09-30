5cb9d4ebf2f694f408ce259ebffa1439ba21a503
EmersonJun <EmersonnJunior2006@gmail.com>
2025-09-30 19:13:54 -0300

    add

--- components/about.tsx
+++ components/about.tsx
@@ -28,7 +28,6 @@ export function About() {
                 {[
                   "JavaScript",
                   "React",
-                  "Next.js",
                   "Python",
                   "PostgreSQL",
                   "MongoDB",
